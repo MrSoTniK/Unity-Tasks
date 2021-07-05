718c799f5d53d2429a70e071f1cac4dbc8fd8f8c
MrSoTniK <48535003+MrSoTniK@users.noreply.github.com>
2021-07-05 06:56:21 +0300

    Update OpenDoor.cs

--- House Signaling/Scripts/OpenDoor.cs
+++ House Signaling/Scripts/OpenDoor.cs
@@ -4,31 +4,24 @@ using UnityEngine;
 
 public class OpenDoor : MonoBehaviour
 {
-    [SerializeField] private float _timeOfMove;
+    [SerializeField] private float _pauseBetweenMoves;
     [SerializeField] private float _degrees;
-    private float _runningTime;   
-    private bool isBreakIn;
-    private Quaternion _positionOfMove;
-    private Quaternion _startPosition;  
-
-    private void Start()
-    {
-        isBreakIn = false;      
-    }
-
-    private void Update()
-    {      
-        if (isBreakIn == true) 
-        {
-            _runningTime += Time.deltaTime;
-            transform.rotation = Quaternion.Lerp(_startPosition, _positionOfMove, _runningTime / _timeOfMove);
-        }     
-    }
+    [SerializeField] private float _degreesRotationStep; 
 
     private void OnTriggerEnter(Collider body)
-    {      
-         _positionOfMove = Quaternion.Euler(0, _degrees, 0);
-         _startPosition = gameObject.transform.rotation;
-         isBreakIn = true;    
+    {             
+        StartCoroutine(Open());
     }    
+
+    private IEnumerator Open() 
+    {
+       float currentDegreesQuantity = 0;
+
+       while(currentDegreesQuantity != _degrees) 
+       {
+            currentDegreesQuantity += _degreesRotationStep;
+            transform.rotation = Quaternion.Euler(0, currentDegreesQuantity, 0);
+            yield return new WaitForSeconds(_pauseBetweenMoves);
+        }            
+    }
 }
