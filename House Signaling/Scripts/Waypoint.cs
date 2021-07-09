02ad6a382fb9513a962b8ede0449476651fbbc27
MrSoTniK <48535003+MrSoTniK@users.noreply.github.com>
2021-07-09 12:39:09 +0300

    Update Waypoint.cs

--- House Signaling/Scripts/Waypoint.cs
+++ House Signaling/Scripts/Waypoint.cs
@@ -4,50 +4,21 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    [SerializeField] private bool _isTrigerExist;
-    [SerializeField] private bool _isAnimationExist;
-    [SerializeField] private bool _isPauseExist;
-
     [SerializeField] private string[] _animationsToSetActive;
     [SerializeField] private string[] _animationsToSetInactive;
-
-    [SerializeField] private string _trigerName;
-    
     [SerializeField] private float _pauseTime;
     [SerializeField] private float _speed;
 
-    [SerializeField] private Vector3 _coordinate;
-
-    public Vector3 Coordinate { get; private set; }
     public float Speed { get; private set; }
     public float PauseTime { get; private set; }
-    public bool IsPauseExist { get; private set; }
+    public string[] AnimationsToSetActive { get; private set; }
+    public string[] AnimationsToSetInactive { get; private set; }
 
     public void Start()
     {
-        Coordinate = _coordinate;
         Speed = _speed;
-        IsPauseExist = _isPauseExist;
         PauseTime = _pauseTime;
-    }
-
-    public void SetAnimations(Animator animator) 
-    {
-        if (_isAnimationExist && _animationsToSetInactive.Length != 0)
-        {
-            for (int i = 0; i < _animationsToSetInactive.Length; i++)
-                animator.SetBool(_animationsToSetInactive[i], false);
-        }
-
-        if (_isAnimationExist && _animationsToSetActive.Length != 0) 
-        {
-            for (int i = 0; i < _animationsToSetActive.Length; i++)
-                animator.SetBool(_animationsToSetActive[i], true);
-        }
-
-        if(_isTrigerExist && _trigerName != null) 
-        {
-            animator.SetTrigger(_trigerName);
-        }
-    }        
+        AnimationsToSetActive = _animationsToSetActive;
+        AnimationsToSetInactive = _animationsToSetInactive;
+    }    
 }
