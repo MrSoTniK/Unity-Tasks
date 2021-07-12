5600d5891bdb41c9cd2f65073e7a9bd71f0be6bf
MrSoTniK <48535003+MrSoTniK@users.noreply.github.com>
2021-07-12 04:45:33 +0300

    Update Waypoint.cs

--- House Signaling/Scripts/Waypoint.cs
+++ House Signaling/Scripts/Waypoint.cs
@@ -3,22 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Waypoint : MonoBehaviour
-{
-    [SerializeField] private string[] _animationsToSetActive;
-    [SerializeField] private string[] _animationsToSetInactive;
+{    
     [SerializeField] private float _pauseTime;
     [SerializeField] private float _speed;
 
     public float Speed { get; private set; }
     public float PauseTime { get; private set; }
-    public string[] AnimationsToSetActive { get; private set; }
-    public string[] AnimationsToSetInactive { get; private set; }
 
     public void Start()
     {
         Speed = _speed;
-        PauseTime = _pauseTime;
-        AnimationsToSetActive = _animationsToSetActive;
-        AnimationsToSetInactive = _animationsToSetInactive;
+        PauseTime = _pauseTime;       
     }    
 }
