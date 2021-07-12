7505c3a27da08e1b6ccbc4f9e1f6ea345b2d8dcf
MrSoTniK <48535003+MrSoTniK@users.noreply.github.com>
2021-07-12 04:44:11 +0300

    Update BanditAnimator.cs

--- House Signaling/Scripts/BanditAnimator.cs
+++ House Signaling/Scripts/BanditAnimator.cs
@@ -8,46 +8,52 @@ using UnityEngine;
 public class BanditAnimator : MonoBehaviour
 {
     [SerializeField] private WaypointsPool _pool;
+    [SerializeField] private AnimationsPool _animPool;
 
     private Waypoint[] _waypoints;
     private int _currentWaypointIndex;
     private Animator _animator;
     private BanditMovement _movement;
+    private Animations[] _animations;
 
     private void Start()
     {
+        _animations = _animPool.GetComponentsInChildren<Animations>();
         _waypoints = _pool.GetComponentsInChildren<Waypoint>();
         _currentWaypointIndex = 0;
         _animator = GetComponent<Animator>();
         _movement = GetComponent<BanditMovement>();
-        SetAnimations(_movement.WaypointIndex);
+        _currentWaypointIndex = SetAnimations(_currentWaypointIndex);
     }
 
     private void Update()
     {
-        if(_movement.WaypointIndex > _currentWaypointIndex) 
+        if(_currentWaypointIndex < _animations.Length) 
         {
-            SetAnimations(_movement.WaypointIndex);
-            _currentWaypointIndex = _movement.WaypointIndex;
-        }                       
+            if (_movement.WaypointIndex == _animations[_currentWaypointIndex].WaypointId)
+                _currentWaypointIndex = SetAnimations(_currentWaypointIndex);
+        }                                
     }
 
-    private void SetAnimations(int index) 
+    private int SetAnimations(int index) 
     {
-        if(_waypoints[index].AnimationsToSetInactive.Length > 0) 
+        if(_animations[index].AnimationsToSetInactive.Length > 0) 
         {
-            foreach (var animationName in _waypoints[index].AnimationsToSetInactive)
+            foreach (var animationName in _animations[index].AnimationsToSetInactive)
             {
                 _animator.SetBool(animationName, false);
             }
         }       
 
-        if(_waypoints[index].AnimationsToSetActive.Length > 0) 
+        if(_animations[index].AnimationsToSetActive.Length > 0) 
         {
-            foreach (var animationName in _waypoints[index].AnimationsToSetActive)
+            foreach (var animationName in _animations[index].AnimationsToSetActive)
             {
                 _animator.SetBool(animationName, true);
             }
-        }       
+        }
+        index = index + 1;
+
+        return index;     
     }
 }
