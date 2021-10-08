1a7988f5eef966c7c6af7f9a187262ece507e005
MrSoTniK <48535003+MrSoTniK@users.noreply.github.com>
2021-10-08 19:21:23 +0300

    Update CharacterCoinsCollector.cs

--- 2D Platformer/Scripts/Character/CharacterCoinsCollector.cs
+++ 2D Platformer/Scripts/Character/CharacterCoinsCollector.cs
@@ -6,20 +6,19 @@ using TMPro;
 public class CharacterCoinsCollector : MonoBehaviour
 {
     [SerializeField] private TMP_Text _coindIndicator;
-
-    public int CoinsQuantity { get; private set; }
+    
     private int _coinsQuantity = 0;
-
-    private void Collect(GameObject coin) 
-    {
-        _coinsQuantity++;
-        Destroy(coin);
-        _coindIndicator.text = _coinsQuantity.ToString();
-    }
 
     private void OnTriggerEnter2D(Collider2D body)
     {
         if (body.TryGetComponent<Coin>(out Coin coin))
             Collect(coin.gameObject);
     }
+
+    private void Collect(GameObject coin) 
+    {
+        _coinsQuantity++;
+        Destroy(coin);
+        _coindIndicator.text = _coinsQuantity.ToString();
+    }  
 }
