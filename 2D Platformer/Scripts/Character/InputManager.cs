05212caff954ae8a6a645d56cd9d323cc7522462
MrSoTniK <48535003+MrSoTniK@users.noreply.github.com>
2021-10-08 22:04:21 +0300

    Add files via upload

---
+++ 2D Platformer/Scripts/Character/InputManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+
+public static class InputManager
+{
+    public static class Axes 
+    {
+        public const string Horizontal = nameof(Horizontal);
+        public const string Jump = nameof(Jump);
+    }
+}
