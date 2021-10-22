96764e28f035fa564244d335f083996b2f8fad1e
MrSoTniK <48535003+MrSoTniK@users.noreply.github.com>
2021-10-22 04:15:44 +0300

    Add files via upload

---
+++ Swamp Attack/Scripts/Enemy/EnemyAnimator.cs
@@ -0,0 +1,10 @@
+
+public static class EnemyAnimator 
+{
+    public static class States
+    {
+        public const string Attack = nameof(Attack);
+        public const string Walk = nameof(Walk);
+        public const string Celebration = nameof(Celebration);
+    }
+}
