fa36afd792e247492dae1ffad97aebce7c030448
MrSoTniK <48535003+MrSoTniK@users.noreply.github.com>
2021-10-22 04:18:17 +0300

    Add files via upload

---
+++ Swamp Attack/Scripts/Player/PlayerAnimator.cs
@@ -0,0 +1,10 @@
+
+public class PlayerAnimator
+{
+    public static class States
+    {
+        public const string Idle = nameof(Idle);
+        public const string Shoot = nameof(Shoot);
+        public const string Celebration = nameof(Celebration);
+    }
+}
