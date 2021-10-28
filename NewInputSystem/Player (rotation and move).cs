48f758e127dc31aae71aaac636b726c2c12a9820
MrSoTniK <48535003+MrSoTniK@users.noreply.github.com>
2021-10-28 21:16:25 +0300

    Create Player (rotation and move).cs

---
+++ NewInputSystem/Player (rotation and move).cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _rotateSpeed;
+
+    private PlayerInput _input;
+    private Vector2 _direction;
+    private Vector2 _rotate;
+    private Vector2 _rotation;
+
+    private void Awake()
+    {
+        _input = new PlayerInput();
+        _input.Enable();     
+    }
+
+    private void Update()
+    {
+        _rotate = _input.Player.Look.ReadValue<Vector2>();
+        _direction = _input.Player.Move.ReadValue<Vector2>();
+
+        Look(_rotate);
+        Move(_direction);
+    }
+
+    private void Move(Vector2 direction)
+    {
+        if (direction.sqrMagnitude < 0.1)
+            return;
+
+        float scaledMoveSpeed = _moveSpeed * Time.deltaTime;
+        Vector3 move = Quaternion.Euler(0, transform.eulerAngles.y, 0) * new Vector3(direction.x, 0, direction.y);
+        transform.position += move * scaledMoveSpeed;
+
+    }
+
+    private void Look(Vector2 rotate)
+    {
+        if (rotate.sqrMagnitude < 0.1)
+            return;
+
+        float scaledRotateSpeed = _rotateSpeed * Time.deltaTime;
+        _rotation.y += rotate.x * scaledRotateSpeed;
+        _rotation.x += Mathf.Clamp(_rotation.x - rotate.y * scaledRotateSpeed, -90, 90);
+        transform.localEulerAngles = _rotation;
+    }
+}
