2ebf1480c4ae927e989004611f43096433aa79ab
MrSoTniK <48535003+MrSoTniK@users.noreply.github.com>
2021-11-16 04:13:50 +0300

    Update HealthChangerButton.cs

--- Smooth HP changer (ver 2)/Scripts/UI/HealthChangerButton.cs
+++ Smooth HP changer (ver 2)/Scripts/UI/HealthChangerButton.cs
@@ -7,26 +7,27 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Button))]
 public abstract class HealthChangerButton : MonoBehaviour
 {
-    [SerializeField] protected HealthBar HealthBar;
     [SerializeField] protected float SpeedOfChange;
     [SerializeField] protected Player Player;
     [SerializeField] private Button _button;
 
     public UnityAction<bool> CoroutineIsWorking;
-    protected UnityAction<float> HealthChanged;
+    public UnityAction<float> HealthChanged;
     protected abstract int ValueOfChange { get; }
+
+    protected delegate bool CheckMethod();
+    protected abstract CheckMethod VerificationHP { get; }
+
     private Coroutine _coroutine;
 
     private void OnEnable()
     {
         _button.onClick.AddListener(delegate { OnButtonClick(ValueOfChange); });
-        HealthChanged += HealthBar.OnHealthChanged;
     }
 
     private void OnDisable()
     {
         _button.onClick.RemoveListener(delegate { OnButtonClick(ValueOfChange); });
-        HealthChanged -= HealthBar.OnHealthChanged;
     }
 
     private void OnButtonClick(int valueOfChange)
@@ -34,7 +35,8 @@ public abstract class HealthChangerButton : MonoBehaviour
         if (_coroutine != null)
             StopCoroutine(_coroutine);
 
-         _coroutine = StartCoroutine(ChangeHP(valueOfChange));
+        if(VerificationHP())
+             _coroutine = StartCoroutine(ChangeHP(valueOfChange));
     }
 
     protected abstract IEnumerator ChangeHP(int valueOfChange);     
