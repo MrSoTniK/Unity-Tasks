963090012bb716e2c8cc2cac731f82c8d6a3b1df
MrSoTniK <48535003+MrSoTniK@users.noreply.github.com>
2021-11-16 04:14:42 +0300

    Update HealButton.cs

--- Smooth HP changer (ver 2)/Scripts/UI/HealButton.cs
+++ Smooth HP changer (ver 2)/Scripts/UI/HealButton.cs
@@ -7,29 +7,27 @@ public class HealButton : HealthChangerButton
     [SerializeField] private int healing;
 
     protected override int ValueOfChange => healing;
+    protected override CheckMethod VerificationHP => Player.CheckHealingPossibility;
 
     protected override IEnumerator ChangeHP(int valueOfChange)
     {
-        if(Player.Health < Player.MaxHealth) 
+        CoroutineIsWorking?.Invoke(true);
+        float currentHealth = Player.Health;
+        float targetHealth = currentHealth + valueOfChange;
+
+        while (currentHealth < targetHealth)
         {
-            CoroutineIsWorking?.Invoke(true);
-            float currentHealth = Player.Health;
-            float targetHealth = currentHealth + valueOfChange;
+            currentHealth = Mathf.MoveTowards(currentHealth, targetHealth, SpeedOfChange * Time.deltaTime);
+            currentHealth = Mathf.Clamp(currentHealth, 0, Player.MaxHealth);
+            HealthChanged?.Invoke(currentHealth);
 
-            while (currentHealth < targetHealth)
+            if (currentHealth == targetHealth)
             {
-                currentHealth = Mathf.MoveTowards(currentHealth, targetHealth, SpeedOfChange * Time.deltaTime);
-                currentHealth = Mathf.Clamp(currentHealth, 0, Player.MaxHealth);
-                HealthChanged?.Invoke(currentHealth);
-
-                if (currentHealth == targetHealth)
-                {
-                    Player.TakeHeal(valueOfChange);
-                    CoroutineIsWorking?.Invoke(false);
-                }
-
-                yield return new WaitForEndOfFrame();
+                Player.TakeHeal(valueOfChange);
+                CoroutineIsWorking?.Invoke(false);
             }
-        }      
+
+            yield return new WaitForEndOfFrame();
+        }
     }
 }
