f426ce9719f5c7077d5ab6f42170ad85ca76c7f1
MrSoTniK <48535003+MrSoTniK@users.noreply.github.com>
2021-11-16 04:13:09 +0300

    Update HealthBar.cs

--- Smooth HP changer (ver 2)/Scripts/UI/HealthBar.cs
+++ Smooth HP changer (ver 2)/Scripts/UI/HealthBar.cs
@@ -10,14 +10,24 @@ public class HealthBar : MonoBehaviour
     [SerializeField] private Slider _slider;
     [SerializeField] private TMP_Text _field;
     [SerializeField] private Player _player;
+    [SerializeField] private HealthChangerButton[] _buttons;
 
     private void OnEnable()
     {
         _slider.value = 1;
         _field.text = ((int)_player.MaxHealth).ToString();
+
+        foreach(var button in _buttons)         
+            button.HealthChanged += OnHealthChanged;        
     }
 
-    public void OnHealthChanged(float currentHealth)
+    private void OnDisable()
+    {
+        foreach (var button in _buttons)       
+            button.HealthChanged -= OnHealthChanged;       
+    }
+
+    private void OnHealthChanged(float currentHealth)
     {             
         _slider.value = (float)currentHealth / _player.MaxHealth;
         _field.text = ((int)currentHealth).ToString();
